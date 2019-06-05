0cbda1be793272dae000308d69d6dbe09da892c0
Andrew Gahov <andrew.gahov@gmail.com>
2019-06-05 12:07:44 +0300

    add websecurity

--- app/src/main/java/net/robomix/blackmoon/controllers/ControllerMainPage.java
+++ app/src/main/java/net/robomix/blackmoon/controllers/ControllerMainPage.java
@@ -6,16 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ControllerMainPage {
 
-    @Autowired
-    private ProjectRepo projectRepo;
+    private final ProjectRepo projectRepo;
+
+    public ControllerMainPage(ProjectRepo projectRepo) {
+        this.projectRepo = projectRepo;
+    }
 
     @GetMapping
     public String main(Map<String, Object> model) {
--- app/src/main/java/net/robomix/blackmoon/domain/repos/ProjectRepo.java
+++ app/src/main/java/net/robomix/blackmoon/domain/repos/ProjectRepo.java
@@ -1,12 +1,10 @@
 package net.robomix.blackmoon.domain.repos;
 
 import net.robomix.blackmoon.domain.models.ProjectModel;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProjectRepo extends CrudRepository<ProjectModel, Long> {
-
     List<ProjectModel> findAll();
 }
